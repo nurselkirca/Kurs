ae0ae3eb89afc3849d58f06ff87ec903a1a97052
Nursel Kırca <nurselkrc27@gmail.com>
2025-07-22 17:41:25 +0300

    TableView

---
+++ UITableView/UITableView/Emoji.swift
@@ -0,0 +1,17 @@
+//
+//  Emoji.swift
+//  UITableView
+//
+//  Created by Nursel Kırca on 22.07.2025.
+//
+
+import Foundation
+
+struct Emoji {
+    var symbol: String
+    var name: String
+    var description: String
+    var usage: String
+}
+
+
